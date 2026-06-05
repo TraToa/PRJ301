2c98b584cf41a44cccd644ff632a585263b8d178
Tran Le Khanh Toan <trantoan.tratoa@gmail.com>
2026-06-05 11:37:03 +0700

    feat(pe.model): Add a DTO class for Registration

---
+++ PBL/PE_SE201448/src/java/pe/model/registration/RegistrationDTO.java
@@ -0,0 +1,54 @@
+package pe.model.registration;
+
+import java.io.Serializable;
+
+public class RegistrationDTO implements Serializable {
+    private String username;
+    private String password;
+    private String fullname; // mapping lastname
+    private boolean role; // mapping isAdmin
+
+    public RegistrationDTO() {
+    }
+
+    public RegistrationDTO(String username, String password, String fullname, boolean role) {
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public boolean isRole() {
+        return role;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public void setRole(boolean role) {
+        this.role = role;
+    }
+
+    
+}
