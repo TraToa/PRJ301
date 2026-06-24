5878667ada68e27c9d5f451613a0a3b9f29fc77b
Tran Le Khanh Toan <trantoan.tratoa@gmail.com>
2026-06-24 22:28:48 +0700

    refactor(pe.controller.MainController): Map the action delete of the form in view to the delete account functional servlet in dispatcher

--- PBL/PE_SE201448/src/java/pe/controllers/MainController.java
+++ PBL/PE_SE201448/src/java/pe/controllers/MainController.java
@@ -20,6 +20,8 @@ public class MainController extends HttpServlet {
     private static final String WELCOME="login.html";
     private static final String LOGIN_CONTROLLER = "LoginServlet";
     private static final String SEARCH_CONTROLLER = "SearchLastNameServlet";
+    private static final String DELETE_ACCOUNT_CONTROLLER = "DeleteAccountServlet";
+
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -45,6 +47,9 @@ public class MainController extends HttpServlet {
                 switch (action) {
                     case "Search":
                         url = SEARCH_CONTROLLER;
+                        break;
+                    case "Delete":
+                        url = DELETE_ACCOUNT_CONTROLLER;
                         break;
                     default:
                         throw new AssertionError();
