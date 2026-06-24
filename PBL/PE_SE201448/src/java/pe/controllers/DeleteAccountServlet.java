f6c8aabc3aaa365f645c0934668066977cdfd45a
Tran Le Khanh Toan <trantoan.tratoa@gmail.com>
2026-06-24 22:21:41 +0700

    feat(pe.controller): Add new functional servlet DeleteAccountServlet

---
+++ PBL/PE_SE201448/src/java/pe/controllers/DeleteAccountServlet.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package pe.controllers;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import pe.model.registration.RegistrationDAO;
+
+/**
+ *
+ * @author TGDD-MSI
+ */
+@WebServlet(name = "DeleteAccountServlet", urlPatterns = {"/DeleteAccountServlet"})
+public class DeleteAccountServlet extends HttpServlet {
+    private static final String ERROR_PAGE = "error.html";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = ERROR_PAGE;
+
+        // 1. Controller gets all necessary request parameters' values
+        String username = request.getParameter("pk");
+        String searchValue = request.getParameter("lastSearchValue");
+
+        try {
+            // 2. Controller calls methods of Model
+            // 2.1. Controller instantiates DAO
+            RegistrationDAO dao = new RegistrationDAO();
+
+            // 2.2 Controller calls methods of DAO
+            boolean result = dao.deleteAccount(username);
+
+            // 3. Controller processes result
+            if (result) { // Deleted successfully
+                // Refresh -> call previous functionality again
+                // --> remind --> add request parameters into URL
+                url = "MainController"
+                        + "?action=Search"
+                        + "&txtSearchValue=" + searchValue;
+            }
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            response.sendRedirect(url);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
